4037907fb27bfa55734866d285b4802b5eef6c98
harish <harishthampyhat@gmail.com>
2023-11-13 16:33:16 +0530

    Changes file name for easy evaluation.

--- test_genvcard.py
+++ test_genvcard.py
@@ -2,7 +2,7 @@ from genvcard import *
 
 
 def test_getdata():
-    path = 'gensheets.csv'
+    path = 'testcsv.csv'
     assert get_data(path) == [["Warren","Tammy","Information officer","tammy.warre@romero.org","(794)913-7421"],
 ["Rodgers","Cheryl","Microbiologist","chery.rodge@cameron-sanders.com","634-332-7960"]]
     
