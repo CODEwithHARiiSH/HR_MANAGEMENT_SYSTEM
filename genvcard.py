9a10371dcce40b3636e37c3952bbe5b0619b41a9
harish <harishthampyhat@gmail.com>
2023-11-13 15:51:23 +0530

    Green:Implements function for generate vcard , test success

--- genvcard.py
+++ genvcard.py
@@ -1,7 +1,5 @@
 import csv
-from sys import argv
-
-script, gensheet= argv
+import os
 #get data from csv file(csv file passed as an argument)
 
 def get_data(gensheet):
@@ -11,3 +9,17 @@ def get_data(gensheet):
          for row in csvreader:
              data.append(row)
     return data
+    
+def gen_vcard(data,vc_count):
+    for i in range(int(vc_count)):
+        lname , fname , designation , email , phone = data[i]
+        content = f"""Name: {fname}
+Full Name: {fname} {lname}
+ORG:Authors, Inc.
+TITLE: {designation}
+TEL;WORK;VOICE: {phone}
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+EMAIL;PREF;INTERNET: {email}
+REV:20150922T195243Z-{i}
+"""
+        return content
