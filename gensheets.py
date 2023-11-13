c711ad7896c1653f0736ce89ae60101caa3fb106
harish <harishthampyhat@gmail.com>
2023-11-13 14:28:33 +0530

    Implements function for generating a csv file.
    The function takes names, email, designation and phone as input and
    generate a csv file using the datas.

---
+++ gensheets.py
@@ -0,0 +1,30 @@
+import csv
+import faker
+from sys import argv
+
+script, gensheet , count= argv
+
+def generate_data(count):
+    f = faker.Faker()
+    names = []
+    for i in range(int(count)):
+        record = []
+        lname = f.last_name()
+        fname = f.first_name()
+        domain = f.domain_name()
+        designation = f.job()
+        email = f"{fname[:5].lower()}.{lname[:5].lower()}@{domain}"
+        phone = f.phone_number()
+        record = [lname, fname, designation, email, phone]
+        names.append(record)
+    return names
+
+def get_csvfile(names):
+    with open(f'{gensheet}', 'w', newline='') as file:
+        writer = csv.writer(file)
+        for i in names:
+            writer.writerow(i)
+
+if __name__ == "__main__":
+    names=generate_data(count)
+    get_csvfile(names)
