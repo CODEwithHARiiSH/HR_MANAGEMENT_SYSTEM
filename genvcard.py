d9c355dae9e28afb5200d551312e9460f3784efb
harish <harishthampyhat@gmail.com>
2023-11-13 15:06:27 +0530

    Red:Implements function for get data by system argument, test fails

--- genvcard.py
+++ genvcard.py
@@ -1,8 +1,12 @@
 import csv
+from sys import argv
+
+script, gensheet= argv
+#get data from csv file(csv file passed as an argument)
 
 def get_data():
     data = []
-    with open("gensheets.csv", 'r') as file:
+    with open(gensheet, 'r') as file:
          csvreader = csv.reader(file)
          for row in csvreader:
              data.append(row)
--- test_genvcard.py
+++ test_genvcard.py
@@ -1,8 +1,8 @@
 from genvcard import *
 
+
 def test_getdata():
-    with open("gensheets.csv", 'r') as file:
-         csvreader = csv.reader(file)
-    assert get_data() == [["Warren","Tammy","Information officer","tammy.warre@romero.org","(794)913-7421"],
+    path = 'gensheets.csv'
+    assert get_data(path) == [["Warren","Tammy","Information officer","tammy.warre@romero.org","(794)913-7421"],
 ["Rodgers","Cheryl","Microbiologist","chery.rodge@cameron-sanders.com","634-332-7960"]]
     
