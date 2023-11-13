95b6c56fd48fdfd1918550d4be841cd019a2ffc8
harish <harishthampyhat@gmail.com>
2023-11-13 15:49:48 +0530

    REd:Implements function for generate vcard , test fails

--- test_genvcard.py
+++ test_genvcard.py
@@ -6,3 +6,17 @@ def test_getdata():
     assert get_data(path) == [["Warren","Tammy","Information officer","tammy.warre@romero.org","(794)913-7421"],
 ["Rodgers","Cheryl","Microbiologist","chery.rodge@cameron-sanders.com","634-332-7960"]]
     
+
+def test_getvcard():
+    data = [["Warren","Tammy","Information officer","tammy.warre@romero.org","(794)913-7421"],
+           ["Rodgers","Cheryl","Microbiologist","chery.rodge@cameron-sanders.com","634-332-7960"]]
+    count = 1
+    assert gen_vcard(data,count) == """Name: Tammy
+Full Name: Tammy Warren
+ORG:Authors, Inc.
+TITLE: Information officer
+TEL;WORK;VOICE: (794)913-7421
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+EMAIL;PREF;INTERNET: tammy.warre@romero.org
+REV:20150922T195243Z-0
+"""
