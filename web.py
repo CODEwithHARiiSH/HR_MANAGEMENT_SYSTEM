e37b4d47d5d8bf9becd4b9c99396fb40e058d43c
harish <harishthampyhat@gmail.com>
2023-12-09 20:19:16 +0530

    Reduced code repeatation

--- web.py
+++ web.py
@@ -21,25 +21,11 @@ def employees():
     users = db.session.execute(query).scalars()
     return flask.render_template("userlist.html", users = users)
 
-cache = {}
-@app.route("/employees/<int:empid>")
-def api_employee_details(empid):
-    if empid in cache:
-        print (f"returning {empid} from cache")
-        return jsonify(cache[empid])
-    else:
+def getdata(empid):
         query = db.select(Employee).where(Employee.id == empid)
         user = db.session.execute(query).scalar()
         query_for_leaves = db.select(func.count(Employee.id)).join(Leave, Employee.id == Leave.employee_id).filter(Employee.id == empid)
         leave = db.session.execute(query_for_leaves).scalar()
-        ids_q = db.select(Employee.id).order_by(Employee.id)
-        ids = db.session.execute(ids_q).fetchall()
-        ids = [id for id, in ids]
-        first_id = ids[0]
-        last_id = ids[len(ids)-1]
-        dates_q = db.select(Leave.date).where(Leave.employee_id == empid)
-        dates = db.session.execute(dates_q).fetchall()
-        dates = [date for date, in dates]
         ret = {"fname" : user.fname,
             "lname" : user.lname,
             "title" : user.designation.designation,
@@ -47,29 +33,44 @@ def api_employee_details(empid):
             "phone" : user.phone,
             "max_leave":user.designation.max_leaves,
             "leave" : leave,
-            "first_id":first_id,
-            "last_id":last_id,
             "id":user.id
             }
-        cache[empid]=ret
-        return jsonify(ret)
+        return ret
+
+cache = {}
+@app.route("/employees/<int:empid>")
+def api_employee_details(empid):
+    try:
+        if empid in cache:
+            print (f"returning {empid} from cache")
+            return jsonify(cache[empid])
+        else:
+            ret = getdata(empid)
+            cache[empid]=ret
+            return jsonify(ret)
+    except:
+        flash('Failed to generate employee data')
+        return redirect(url_for('employees'))
 
 
 @app.route('/add_leaves/<int:empid>', methods=['GET', 'POST'])
 def add_leaves(empid):
-    query = db.select(Employee.fname).where(Employee.id == empid)
-    user = db.session.execute(query).scalar()
-    if request.method == 'POST':
-        date = request.form['date']
-        reason = request.form['reason']
+    ret = getdata(empid)
+    try:
+        if request.method == 'POST':
+            date = request.form['date']
+            reason = request.form['reason']
 
-        leave = Leave(date=date,
-                                employee_id=empid,
-                                reason=reason)
-        db.session.add(leave)
-        db.session.commit()
-        del cache[empid]
-        flash(f'Leave added successfully for {user}', 'success')
+            leave = Leave(date=date,
+                                    employee_id=empid,
+                                    reason=reason)
+            db.session.add(leave)
+            db.session.commit()
+            del cache[empid]
+            flash('Leave added successfully for' + ret['fname'], 'success')
+            return redirect(url_for('employees'))
+    except:
+        flash('Failed to add leave for ' + ret['fname'],'Already Taken')
         return redirect(url_for('employees'))
 
 
