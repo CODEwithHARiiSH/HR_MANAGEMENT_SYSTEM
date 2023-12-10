6ee9fc4f0af990e65c8cf2aae49fc3485378d920
harish <harishthampyhat@gmail.com>
2023-12-10 11:48:54 +0530

    Web page looks nice now
    1.Employee details changed into a card
    2.Changed index image
    3.Adds an image to card
    4.Changed span messages

--- static/jsons/main.js
+++ static/jsons/main.js
@@ -11,47 +11,26 @@ function gotEmployees(data , msg) {
     console.log(data);
     $("span.info")[0].innerHTML = "Done &#9745;";
     $("div#userdetails").data("empid", data.id);
-    $("#userdetails")[0].innerHTML = `<h1> ${data.fname}  ${data.lname}</h1>
-    <hr>
-    <h5>Designation :  ${data.title} </h5>
-    <table>
-      <tr>
-        <th> First name :</th>
-        <td> ${data.fname}</td>
-      </tr>
-      <tr>
-        <th> Last name :</th>
-        <td> ${data.lname}</td>
-      </tr>
-      <tr>
-        <th> Email :</th>
-        <td> ${data.email}</td>
-      </tr>
-
-      <tr>
-        <th> Phone :</th>
-        <td> ${data.phone}</td>
-      </tr>
-      </table>
-      <hr>
-      <h2 style="text-align: center;"> Leave details</h2>
-          <table>
-              <tr>
-                <th> Leave taken :</th>
-                <td> ${data.leave}</td>
-              </tr>
-              <tr>
-                <th> Maximum leave allowed :</th>
-                <td> ${data.max_leave}</td>
-              </tr>
-              <tr>
-                <th> Remaining leaves :</th>
-                <td> ${data.max_leave - data.leave}</td>
-              </tr>
-            </table>
-            <hr>
-            <br>
-            ${renderForm(data)}
+    $("#userdetails")[0].innerHTML = `
+        <h1>${data.fname} ${data.lname}</h1>
+        <h5>Designation: ${data.title}</h5>
+        <hr>
+        <div>
+            <strong>First name:</strong> ${data.fname}<br>
+            <strong>Last name:</strong> ${data.lname}<br>
+            <strong>Email:</strong> ${data.email}<br>
+            <strong>Phone:</strong> ${data.phone}<br>
+        </div>
+        <hr>
+        <h2 style="text-align: center;">Leave details</h2>
+        <div>
+            <strong>Leave taken:</strong> ${data.leave}<br>
+            <strong>Maximum leave allowed:</strong> ${data.max_leave}<br>
+            <strong>Remaining leaves:</strong> ${data.max_leave - data.leave}<br>
+        </div>
+        <hr>
+        <br>
+        ${renderForm(data)}
 `;
 
 }
@@ -68,7 +47,7 @@ else {
   return `<form action="/add_leaves/${data.id}" method="post">
 
   <input type="date" id="date" name="date" placeholder="Date" ><br>
-  <textarea id="reason" name="reason" rows="4" cols="50" placeholder="Reason" ></textarea><br>
+  <textarea id="reason" name="reason" rows="4" cols="30" placeholder="Reason" ></textarea><br>
 
   <div class="col-auto">
     <button type="submit" class="btn btn-primary mb-3">Submit</button>
